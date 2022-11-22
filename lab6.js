699272b24641e271d8dc30d09b78a75aec34fdf4
брюзгин виктор викторович <bryuzgin02@mail.ru>
2022-11-22 09:39:03 +0700

    Alert

--- lab6.js
+++ lab6.js
@@ -19,3 +19,25 @@ function setEnglish() {
     document.getElementById('your-name').innerHTML = 'Your name';
     document.getElementById('input-name').placeholder = 'enter your name';
 }
+
+function showAlert() {
+    let userName = document.getElementById('input-name').value;
+    let userSurname = document.getElementById('input-surname').value;
+    let userAge = document.getElementById('input-age').value;
+    alert('Привет, ' + userName + ' ' + userSurname + '\n' + 'Ваш возраст: ' + userAge);
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b= Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('user-data').style.backgroundColor = color;
+}
+
+function showAlert2() {
+    alert('Это я делал');
+    document.getElementById('student').style.backgroundColor = 'red';
+}
+
+function showAlert3() {
+    alert('Это я делал');
+    document.getElementById('footer').style.backgroundColor = 'red';
+}
